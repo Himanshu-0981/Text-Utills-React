63ee3def0180ca5117ade9634d33c4075edb8df2
Himanshu <kumarhimanshu2219@gmail.com>
2021-09-18 21:15:11 +0530

    darkmode added

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import './App.css';
+// import About from './components/About';
 import Navbar from './components/Navbar';
 import TextForm from './components/TextForm';
 
@@ -6,8 +7,9 @@ function App(){
     return (
         <div>
           <Navbar title = "Text Utils" aboutText = "About"/>
-          <div className="container pt-3 text-center">
+          <div className="container pt-3">
           <TextForm title="Enter the text to anylize Below"/>
+          {/* <About/> */}
           </div>
         </div>
     )
