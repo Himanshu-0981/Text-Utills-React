4efc04ca595386224d6eb72ad925390b22451541
Himanshu <kumarhimanshu2219@gmail.com>
2021-09-18 19:04:47 +0530

    final text utill functionality added

--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -17,25 +17,28 @@ export default function TextForm(props) {
         setText (clear);
     }
 
-    const onChange = (event) =>{
-        setText(event.target.value)
+    const onChange = (event) => {
+        setText (event.target.value)
     }
-    let someText = ""
-    const [text , setText] = useState(someText);
 
+    const alertFun = () =>{
+        alert("Hello there")
+    }
+
+    const [text , setText] = useState("");
     
-
     return (
         <>
         <div className="container">
             <h1>{props.title}</h1>
             <div className="mb-3">
-                <textarea  className="form-control"  value = {text}  onChange={onChange} id="myBox"rows="6"></textarea>
+                <textarea className="form-control"  value = {text}  onChange={onChange} id="myBox"rows="6"></textarea>
             </div>
             <div className="container d-flex justify-content-between">
             <button className="btn-primary" onClick={onClickUp} >Convert To UpperCase</button>
             <button className="btn-primary" onClick={clear} >Clear All</button>
             <button className="btn-primary" onClick={onClickLow} >Convert To LowerCase</button>
+            <button className="btn-primary" onClick={alertFun} >Click here to get a notification</button>
             </div>
         </div>
         <div className="container">
